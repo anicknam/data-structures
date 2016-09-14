77a16559672fc0ce1a3a06aaeee11a64c628b5fa
anicknam <a.nicknam@gmail.com>
2016-09-14 15:40:50 -0700

    finished enqueue about to write deque

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,20 +1,37 @@
 var Queue = function() {
   var someInstance = {};
-  var index = 0;
+  var queSize = 0;
+  var countAdd = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  //ths is the key to the first queued item
+  storage.first = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //increase que size
+    queSize++;
+    //create que item
+    storage[countAdd] = {data: value, next: undefined};
+    if (countAdd > 0) {
+      //attatch next of previous que item to this
+      storage[countAdd-1].next = countAdd;
+    }
+    countAdd++;
   };
 
   someInstance.dequeue = function() {
+    if (queSize !== 0) {
+      //something
+      queSize--;
+    }
   };
 
   someInstance.size = function() {
-    return index;
+    return queSize;
   };
 
   return someInstance;
