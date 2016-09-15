1627b90a16f58ca6c267ef6648029099c1b3855a
anicknam <a.nicknam@gmail.com>
2016-09-15 15:11:16 -0700

    finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,15 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.tail) {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    } else {
+      list.head = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      var temp = list.head.value;
+      list.head = list.head.next;
+      return temp;
+    } 
   };
 
   list.contains = function(target) {
+    var currNode = list.head;
+    while (currNode) {
+      
+      if (target === currNode.value) {
+        return true;
+      } else {
+        currNode = currNode.next;
+      }
+
+    }
+    return false;
   };
 
   return list;
+
 };
 
 var Node = function(value) {
