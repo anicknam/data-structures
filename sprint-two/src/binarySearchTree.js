22df2577e15f909af9456f305b3b3e4410ce6d59
anicknam <a.nicknam@gmail.com>
2016-09-17 10:15:57 -0700

    finished bst

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -7,19 +7,46 @@ var BinarySearchTree = function(value) {
 // A .contains() method, which accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
 // A .depthFirstLog() method, which accepts a callback and executes it on every value contained in the tree.
   var node = Object.create(methods);
+  node.value = value;
   node.left = undefined;
   node.right = undefined;
   return node;
 };
 
 var methods = {
-  insert: function() {
+  insert: function(val) {
 
-  }, 
-  contains: function() {
+    // inner function that checks the value and checks the target child 
+    // and if the target child is undefined, makes that value the target child
+    // and if not, repeat!
 
+    if (val > this.value) {
+      (this.right) ? this.right.insert(val) : this.right = BinarySearchTree(val);
+    } else {
+      (this.left) ? this.left.insert(val) : this.left = BinarySearchTree(val);
+    }
   },
-  depthFirstLog: function() {
+
+  contains: function(val) {
+
+    if (val === this.value) {
+      return true;
+    } else {
+      if (val > this.value) {
+        return (this.right) ? this.right.contains(val) : false; 
+      } else {
+        return (this.left) ? this.left.contains(val) : false; 
+      }
+    }
+  },
+
+  depthFirstLog: function(cb) {
+    cb(this.value);
+    if (this.right) {
+      this.right.depthFirstLog(cb);
+    } else if (this.left) {
+      this.left.depthFirstLog(cb);
+    }
 
   }
 };
@@ -28,3 +55,10 @@ var methods = {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// insert: O(log n)
+// contains: O(log n)
+// depthFirstLog: O(n)
+
+
+
