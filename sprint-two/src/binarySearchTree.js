a199794045063dbce72012c88ded427629eddfb0
anicknam <a.nicknam@gmail.com>
2016-09-16 20:57:47 -0700

    Started BinarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,27 @@
 var BinarySearchTree = function(value) {
+
+
+// A .left property, a binary search tree (BST) where all values are lower than than it the current value.
+// A .right property, a BST where all values are higher than than it the current value.
+// A .insert() method, which accepts a value and places in the tree in the correct position.
+// A .contains() method, which accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
+// A .depthFirstLog() method, which accepts a callback and executes it on every value contained in the tree.
+  var node = Object.create(methods);
+  node.left = undefined;
+  node.right = undefined;
+  return node;
+};
+
+var methods = {
+  insert: function() {
+
+  }, 
+  contains: function() {
+
+  },
+  depthFirstLog: function() {
+
+  }
 };
 
 
